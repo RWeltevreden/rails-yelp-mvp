892a99e6ef0f1a64b755d491f44a57fe901b84fd
Romee Weltevreden <romeeweltevreden@hotmail.com>
2024-02-21 17:17:51 +0100

    added code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,33 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # resources :restaurants
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
+
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  post "restaurants", to: "restaurants#create"
+  patch "restaurants/:id", to: "restaurants#update"
+
+
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+
+  # GET "restaurants/38"
+
+
+
+  # get "restaurants/:id/reviews/new"
+  # POST "restaurants/38/reviews"
+  # post "restaurants/:id/reviews", to:
+
+
+  #  post "tasks", to: "tasks#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category:"belgian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category:"italian"}
+mcdonalds = {name: "McDonald's", address: "Muntplein, Amsterdam", category:"french"}
+subway = {name: "Subway", address: "Rembrandtplein, Amsterdam", category:"french"}
+papa_johns = {name: "Papa John's", address: "Dam, Amsterdam", category:"belgian"}
+
+[dishoom, pizza_east, mcdonalds, subway, papa_johns].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
